c6a27e2c80e98c7a701fce24ae6b2f3098ecd8d3
An0rak <phos4es@gmail.com>
2018-03-24 00:08:12 -0400

    uuuhg parseing

--- components/FolderView.android.js
+++ components/FolderView.android.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import { View,Text, } from 'react-native';
+import FolderCard from './FolderCard';
 
+var DomParser = require('react-native-html-parser').DOMParser;
 export default class FolderView extends React.Component {
+  constructor(props){
+    super(props);
+  }
+
+  _getURI = URI => {
+
+  }
+
   render(){
     return(
-      <View>
-        <Text>Heello</Text>
+      <View style={{flex: 1,}}>
+        <FolderCard folderName={"Test Folder"} onPress={console.log("Press")}
       </View>
     );
   }
--- screens/HomeScreen.android.js
+++ screens/HomeScreen.android.js
@@ -1,20 +1,29 @@
 import React from 'react';
 import { Text, View, TouchableOpacity, StyleSheet, } from 'react-native';
 import FolderCard from '../components/FolderCard';
+import FolderView from '../components/FolderView';
 
+var DOMParser = require('react-native-html-parser').DomParser;
 export default class HomeScreen extends React.Component {
   static navigationOptions = {
     title: 'Home',
   };
 
   _openFolder = () => {};
-
+  _parse = response => {
+    let doc =  new DOMParser().parseFromString('<td></td>');
+    return doc;
+  }
+  _getURI = URI => {
+    console.log("Clicked");
+    fetch(URI,{ method: 'GET', }).then((response) => response.json()).then((responseJSON) => console.log(responseJSON));
+  }
   render(){
     const { navigate } = this.props.navigation;
     return(
       <View style={{flex: 1,}}>
-        <FolderCard folderName="Test Folder" onPress={this._openFolder}/>
-        <View style={styles.spacer}/>
+        <Text>asdf</Text>
+        <FolderView />
       </View>
     );
   }
