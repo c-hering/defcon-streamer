d9ab313ff185d0f2266a48222cf60e9569f422ec
An0rak <phos4es@gmail.com>
2018-03-29 17:12:23 -0400

    asdf

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ import { StyleSheet, Text, View } from 'react-native';
 import { StackNavigator, } from 'react-navigation';
 import HomeScreen from './screens/HomeScreen';
 
+
+//Entering point of the app, centers of naviagtion for the app
 const App = StackNavigator({
   Home: { screen: HomeScreen },
 });
